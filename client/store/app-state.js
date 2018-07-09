622205a46ff77445452fc03321380cbfe22c870f
kuaifuping <975083196@qq.com>
2018-07-09 16:30:05 +0800

    master stash恢复提交

--- client/store/app-state.js
+++ client/store/app-state.js
@@ -8,7 +8,7 @@ export class AppState {
   @observable count = 0
   @observable name = 'kuaifuping'
   @computed get msg() {
-    return `${this.name} say count is ${this.count}`
+    return `${this.name} say count1 is ${this.count}`
   }
   @action add() {
     this.count += 1
